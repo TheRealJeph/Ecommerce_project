d19ae56a71b18ab93a0d9da65d79de0a473ea860
TheRealJeph <jephcham@gmail.com>
2026-09-06 11:59:32 +0200

    first change to push

---
+++ backend/store/views.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Category, Product, userProfile, Order, OrderItem
+from .serializers import CategorySerializer, ProductSerializer
+
+
+@api_view(['GET'])
+def get_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_categories(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
+
